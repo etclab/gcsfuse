3a13807d27f92a6c115498dc451f75fa639e1e65
Aashutosh Poudel <apoudel01@wm.edu>
2025-06-10 11:14:38 -0400

    keep original config intact

--- smh/mount.sh
+++ smh/mount.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
 
+# --foreground
+# --tmp-dir
+
+rm -rf smh/run/filecache
+rm -f smh/logs/log.json
+
+
 ./gcsfuse \
     --config-file smh/conf/config.yaml \
     --implicit-dirs \
-   atp-cmek mnt
+    --debug_fuse_errors \
+    --debug_fuse \
+    --debug_fs \
+    --debug_gcs \
+    --debug_http \
+    --debug_http \
+   atp-2341-cmek mnt
